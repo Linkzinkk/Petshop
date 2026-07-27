d9ed78baefa600a5e50e619dd7d2504d0c0c0e97
lucas mello <165729756+Linkzinkk@users.noreply.github.com>
2026-07-27 17:02:55 -0300

    Delete Veterinario.ts

--- Veterinario.ts
+++
@@ -1,9 +0,0 @@
-export class Veterinario {
-    constructor(
-        public id: number,
-        public nome: string,
-        public crmv: string,
-        public especialidade: string
-
-    ) { }
-}
