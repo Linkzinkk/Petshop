1c04b3624583efe22887cbc06f93221867a6abfb
lucas mello <165729756+Linkzinkk@users.noreply.github.com>
2026-07-27 17:02:44 -0300

    Delete Servico.ts

--- Servico.ts
+++
@@ -1,9 +0,0 @@
-export class Servico {
-    constructor(
-        public id: number,
-        public descricao: string,
-        public valor: number,
-        public duracaoMinutos: number
-
-    ) { }
-}
